abf3ecf63b1b3521d2faa37e812f6b3fbafc5f43
wiinuk <wii.nukegara@gmail.com>
2019-02-08 23:36:59 +0900

    update paket.template

--- tests/OpenTK.TypeProviders.Tests/OpenTK.TypeProviders.Tests.fs
+++ tests/OpenTK.TypeProviders.Tests/OpenTK.TypeProviders.Tests.fs
@@ -2,9 +2,19 @@ module OpenTK.TypeProviders.Tests
 open NUnit.Framework
 open OpenTK.TypeProviders
 open OpenTK.FSharp.OpenGL
+open OpenTK.Graphics.OpenGL
+open OpenTK
+
 
 let test() =
     let x = new ShaderProgram<"attribute float v1; uniform vec4 v2; void main(void) { gl_Position = vec4(0.0); }", "void main(void) { gl_FlagColor = vec4(1.0); }">()
 
-    let v1: float ShaderAttribute = x.Variables.v1
-    v1
+    let (ShaderAttribute v1): float32 ShaderAttribute = x.Variables.v1
+    Assert.AreEqual(typeof<float32>.TypeHandle, v1.definition.cliType)
+    Assert.AreEqual("float", v1.definition.meta.openGLTypeName)
+    Assert.AreEqual("v1", v1.definition.meta.variableName)
+    Assert.AreEqual(1, v1.definition.spec.vartexElementCount)
+    Assert.AreEqual(VertexAttribPointerType.Float, v1.definition.spec.vertexType)
+
+    let (ShaderUniform v2): Vector4 ShaderUniform = x.Variables.v2
+    ()
